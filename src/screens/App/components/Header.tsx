55bf348eb5bd97c7436a572fefa6412eafe4a807
besizzo <besizzo@gmail.com>
2022-05-09 09:23:16 +0300

    header position adjusted

--- src/screens/App/components/Header.tsx
+++ src/screens/App/components/Header.tsx
@@ -18,16 +18,25 @@ const headerStyles = {
 export const Header: React.FC = () => {
   return (
     <Box sx={headerStyles} component='header'>
-      <Box sx={{ maxWidth: '600px' }}>
-        <Link to={routeConstants.MAIN.route}>
-          <img src={logo} alt='logo' style={{ height: '10vh' }} />
-        </Link>
-        <Link to={routeConstants.FAVOURITES.route}>
-          <img
-            src={star}
-            alt='star'
-            style={{ height: '40px', borderRadius: '50%', cursor: 'pointer', position: 'absolute', marginLeft: 85, marginTop: 42 }} />
-        </Link>
+      <Box sx={{
+        maxWidth: '600px',
+        display: 'grid',
+        gridAutoColumns: '1fr',
+        gap: 1,
+      }}>
+        <Box sx={{ gridRow: '1', gridColumn: '3 / 6' }}>
+          <Link to={routeConstants.MAIN.route}>
+            <img src={logo} alt='logo' style={{ height: '10vh' }} />
+          </Link>
+        </Box>
+        <Box sx={{ gridRow: '1', gridColumn: '7 / 8', margin: 'auto' }}>
+          <Link to={routeConstants.FAVOURITES.route}>
+            <img
+              src={star}
+              alt='star'
+              style={{ height: '40px', borderRadius: '50%', cursor: 'pointer' }} />
+          </Link>
+        </Box>
       </Box>
     </Box>
   );
